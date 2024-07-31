abc64f40f565712c07915aff78cc4de9dc55228c
Kevin Lim <kevinlbthai79@gmail.com>
2024-07-31 18:52:05 +0800

    Updated method in ToAbstractionsTest.java to refactor the code for readability.

--- src/test/java/RefactoringToAbstractionsTest.java
+++ src/test/java/RefactoringToAbstractionsTest.java
@@ -15,23 +15,36 @@ public class RefactoringToAbstractionsTest {
         WebDriver driver = new ChromeDriver();
         driver.get("https://eviltester.github.io/simpletodolist/todolists.html");
 
+        createToDoList(driver, "My First Todo List");
+
+        WebElement renderName;
+        String listDataId = "My-First-Todo-List";
+
+        renderName = getToDoListNameElement(driver, listDataId);
+
+        Assertions.assertEquals(listDataId,
+                renderName.getText());
+
+        driver.close();
+    }
+
+    private static WebElement getToDoListNameElement(WebDriver driver, String listDataId) {
+        WebElement renderName;
+        final WebElement listitem = driver.findElement(
+                By.cssSelector(
+                        "li[data-id='" + listDataId + "']"));
+
+        renderName = listitem.findElement(
+                By.tagName("label"));
+        return renderName;
+    }
+
+    private static void createToDoList(WebDriver driver, String listName) {
         final WebElement todoListNameInput =
                 driver.findElement(
                         By.cssSelector("input.new-todo-list"));
 
         todoListNameInput.sendKeys(
-                "My First Todo List" + Keys.ENTER);
-
-        final WebElement listitem = driver.findElement(
-                By.cssSelector(
-                        "li[data-id='My-First-Todo-List']"));
-
-        final WebElement rendername = listitem.findElement(
-                By.tagName("label"));
-
-        Assertions.assertEquals("My-First-Todo-List",
-                rendername.getText());
-
-        driver.close();
+                listName + Keys.ENTER);
     }
 }
