26001ef48c1a4e7118178dc79d992070f063298e
Kevin Lim <kevinlbthai79@gmail.com>
2024-08-02 21:54:48 +0800

    Check in new java class file for chapter 4 practices.

---
+++ src/main/java/AdminViewPage.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class AdminViewPage {
+    private final WebDriver driver;
+
+    public AdminViewPage(final WebDriver driver) {
+        this.driver = driver;
+        if(!driver.getTitle().contentEquals("Todo Admin View")){
+            throw new RuntimeException("Not on Admin Page");
+        }
+    }
+
+    public int countLists() {
+        return driver.findElements(
+                By.cssSelector("ul#reportlist li")).size();
+    }
+}
